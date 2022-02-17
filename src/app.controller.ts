b7370de3811b55990a7775041b41e583485c9f29
Jabi <michal.jabi@gmail.com>
2022-02-17 10:23:32 +0100

    chore(nest): initial scaffold

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHello(): string {
+    return 'Hello !';
+  }
+
+  @Get('/user')
+  getSample() {
+    return { name: 'Michal' };
+  }
+}
