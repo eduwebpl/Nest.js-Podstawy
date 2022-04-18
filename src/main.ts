f80a4125481627d883e0ee3de15d73607ac876ca
Adam Gospodarczyk <adam@overment.com>
2022-04-18 20:01:43 +0200

    25-finish

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './filters/http-exception.filter';
+import { GlobalExceptionFilter } from './filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT);
   console.log(`My server is running on port ${process.env.PORT}`);
 }
