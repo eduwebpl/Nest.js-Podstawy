cbf7d357c53d1a717f49d00bc57c0b171fd8f01a
Adam Gospodarczyk <adam@overment.com>
2022-04-29 19:40:50 +0200

    25-finish

---
+++ src/filters/database-exception.filter.ts
@@ -0,0 +1,27 @@
+import { Catch, ExceptionFilter, HttpStatus } from '@nestjs/common';
+import { QueryFailedError, TypeORMError } from 'typeorm';
+import { ArgumentsHost } from '@nestjs/common/interfaces/features/arguments-host.interface';
+import { PostgresError } from 'pg-error-enum';
+import { Response } from 'express';
+
+@Catch(TypeORMError)
+export class DatabaseExceptionFilter implements ExceptionFilter {
+  catch(exception: TypeORMError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    let statusCode = HttpStatus.BAD_REQUEST;
+    let message = 'Database error';
+
+    if (exception instanceof QueryFailedError) {
+      if (exception.driverError?.code === PostgresError.UNIQUE_VIOLATION) {
+        message = 'Duplicate entry';
+        statusCode = HttpStatus.CONFLICT;
+      }
+    }
+
+    response.status(statusCode).json({
+      statusCode,
+      message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DatabaseExceptionFilter } from './filters/database-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new DatabaseExceptionFilter());
   await app.listen(process.env.PORT);
   console.log(`My server is running on port ${process.env.PORT}`);
 }
